26d801aa0009eb143d82e1a473b12d978b5931a5
Javier Eduardo Figueroa Torrijo <82825885+JaIng07@users.noreply.github.com>
2024-04-09 20:45:13 -0500

    Add files via upload

---
+++ TiendaOnline/src/main/java/com/pweb/tiendaonline/dtos/itemPedido/ItemPedidoMapper.java
@@ -0,0 +1,24 @@
+package com.pweb.tiendaonline.dtos.itemPedido;
+
+import com.pweb.tiendaonline.entities.ItemPedido;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ItemPedidoMapper {
+
+    ItemPedidoMapper INSTANCE = Mappers.getMapper( ItemPedidoMapper.class );
+
+    ItemPedidoDto itemPedidoEntityToItemPedidoDto(ItemPedido itemPedido);
+
+    @InheritInverseConfiguration
+    ItemPedido itemPedidoDtoToItemPedidoEntity(ItemPedidoDto itemPedidoDto);
+
+    ItemPedido itemPedidoSaveDtoToItemPedidoEntity(ItemPedidoToSaveDto itemPedidoDto);
+
+    ItemPedido itemPedidoToShowDtoToItemPedidoEntity(ItemPedidoToShowDto itemPedidoToShowDto);
+
+    ItemPedidoToShowDto itemPedidoEntityToItemPedidoToShowDto(ItemPedido itemPedido);
+}
---
+++ TiendaOnline/src/main/java/com/pweb/tiendaonline/dtos/pedido/PedidoResponse.java
@@ -0,0 +1,4 @@
+package com.pweb.tiendaonline.dtos.pedido;
+
+public class PedidoResponse {
+}
